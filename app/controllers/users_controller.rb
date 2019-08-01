8f8fa01209c9c10dc11b2d7b2de9e761872585bd
ty <uw.ty0cu21@gmail.com>
2019-08-01 04:57:23 +0000

    favorite, unfavorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -29,13 +29,19 @@ class UsersController < ApplicationController
     
   def followings
     @user = User.find(params[:id])
-    @followings = @user.followings.page(params[:page])
+    @followings = @user.followings.order(id: :desc).page(params[:page])
     counts(@user)
   end
   
   def followers
-    @user =User.find(params[:id])
-    @followers = @user.followers.page(params[:page])
+    @user = User.find(params[:id])
+    @followers = @user.followers.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @fav_microposts = @user.fav_microposts.order(id: :desc).page(params[:page])
     counts(@user)
   end
   
